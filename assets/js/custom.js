1517a423cfa6cb1fd52d326912f89f9ed0664789
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-06-16 08:44:00 +0600

    v 2.0

---
+++ assets/js/custom.js
@@ -0,0 +1,57 @@
+var counterOne = document.querySelector(".one")
+var countertwo = document.querySelector(".two")
+var counterthree = document.querySelector(".three")
+var counterFour = document.querySelector(".four")
+
+var countOne = 0
+var countTwo = 0
+var countThree = 0
+var countFour = 0
+
+function counterUp(){
+    counterOne.innerHTML = countOne
+    countOne++
+    if(countOne > counterOne.dataset.textone){
+
+        clearInterval(stop)
+    }
+}
+var stop = setInterval(function(){
+    counterUp()
+},8)
+
+function counterUpTwo(){
+    countertwo.innerHTML = countTwo
+    countTwo++
+    if(countTwo > countertwo.dataset.texttwo){
+        clearInterval(thamo)
+    }
+
+}
+var thamo = setInterval(function(){
+    counterUpTwo()
+},8)
+
+function counterUpThree(){
+    counterthree.innerHTML = countThree
+    countThree++
+    if(countThree > counterthree.dataset.textthree){
+        clearInterval(tham)
+    }
+}
+var tham = setInterval(function(){
+    counterUpThree()
+},60)
+
+
+function counterUpFour(){
+    counterFour.innerHTML = countFour
+    countFour++
+    if(countFour > counterFour.dataset.textfour){
+        clearInterval(thams)
+    }
+}
+
+var thams = setInterval(function(){
+    counterUpFour()
+},50)
